992fa6a9c56aafd626511c9ef4ac71e9c324189a
Otavio Santana <otaviopolianasantana@gmail.com>
2019-07-24 07:37:05 -0300

    fixes speaker

--- conference/src/main/java/org/jespanol/conference/Speaker.java
+++ conference/src/main/java/org/jespanol/conference/Speaker.java
@@ -14,12 +14,12 @@ public class Speaker {
     @Column
     private String name;
 
-    public void setId(Integer id) {
-        this.id = id;
+    public Integer getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getName() {
+        return name;
     }
 
     @Override
