b3e5b36cfba7f1af30434e2ded33c4ca9f6a5da9
Otavio Santana <otaviopolianasantana@gmail.com>
2019-07-30 08:52:45 -0300

    fixes improts

--- client/src/main/java/org/jespanol/client/speaker/SpeakerController.java
+++ client/src/main/java/org/jespanol/client/speaker/SpeakerController.java
@@ -23,13 +23,10 @@ import javax.mvc.Controller;
 import javax.mvc.Models;
 import javax.mvc.View;
 import javax.ws.rs.BeanParam;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.util.Optional;
 
 @Controller
