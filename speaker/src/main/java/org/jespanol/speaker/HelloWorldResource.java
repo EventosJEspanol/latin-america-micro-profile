f951042d6e1b8da05c9b1f91bf6197c99931d0d0
Otávio Santana <otaviopolianasantana@gmail.com>
2019-08-03 16:12:15 -0300

    Revert "Deletes unnecessary resource."

---
+++ speaker/src/main/java/org/jespanol/speaker/HelloWorldResource.java
@@ -0,0 +1,17 @@
+package org.jespanol.speaker;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("")
+@RequestScoped
+public class HelloWorldResource {
+
+    @GET
+    @Produces("text/plain")
+    public String doGet() {
+        return "hello from Speaker";
+    }
+}
